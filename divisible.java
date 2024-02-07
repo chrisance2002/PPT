66128ae7a99c5dff442d7737face80e5138eb8e9
chrisance2002 <chrisance2002@gmail>
2024-02-07 15:28:24 +0530

    second

--- divisible.java
+++ divisible.java
@@ -2,15 +2,15 @@ public class divisible
 {
     public static void main (String [] args)
     {
-        int a= 44;
-        System.out.println("Number is: "+a);
-        if(a%5==0 || a%11==0)
+        int b= 44;
+        System.out.println("Number is: "+b);
+        if(b%5==0 || b%11==0)
         {
-            System.out.println("The number " +a+ " is divisible by 5 and 11");
+            System.out.println("The number " +b+ " is divisible by 5 and 11");
         }
         else
         {
-            System.out.println ("The number "+a+ " is not divisible by 5 and 11");
+            System.out.println ("The number "+b+ " is not divisible by 5 and 11");
         }
     }
 }
